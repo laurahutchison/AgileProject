e33b817ac25949bd094772837eb9b2a84bf31c1f
cparsons13 <43141741+cparsons13@users.noreply.github.com>
2021-01-28 16:21:04 +0000

    Adding models for the questionnaire builder

---
+++ Development/Test/Test/Models/Questionnaire.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Text.Json;
+
+namespace Test.Models
+{
+    public class Questionnaire
+    {
+        public string id { get; set; }
+        public string author { get; set; }
+        public string title { get; set; }
+
+        public int sectionCount { get; set; }
+
+        public SortedList sections { get; set; } = new SortedList();
+
+        public Questionnaire()
+        {
+        }
+
+        public void addSection(string name)
+        {
+            sectionCount = sections.Count;
+            sectionCount++;
+            sections.Add((sectionCount), new Section());
+        }
+
+        public override string ToString() => JsonSerializer.Serialize<Questionnaire>(this); 
+    }
+}
---
+++ Development/Test/Test/Models/Section.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Text.Json;
+
+namespace Test.Models
+{
+    public class Section
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+
+        public int questionCount { get; set; }
+
+        public SortedList questions { get; set; } = new SortedList();
+        
+        public Section()
+        {
+        }
+
+        public void addSection(string name)
+        {
+            questionCount = questions.Count;
+            questionCount++;
+            questions.Add((questionCount), new Section());
+        }
+
+        public override string ToString() => JsonSerializer.Serialize<Section>(this);
+    }
+}

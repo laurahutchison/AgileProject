e40e53a083d66d22af1418459cb8871316cb8026
Gina Fenton <43143955+ginafent@users.noreply.github.com>
2021-02-04 17:30:26 +0000

    gina and shanna login stuff

---
+++ Development/Test/Pages/IndexLogIn.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.Pages
+{
+    public class IndexOutModel : PageModel
+    {
+        string connectionString = @"Server=silva.computing.dundee.ac.uk;Database=20agileteam9db;Uid=20agileteam9;Pwd=3489.at9.9843;";
+
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexOutModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
+
+

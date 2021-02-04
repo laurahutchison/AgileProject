00834d06aa60bf86326050ea41656742f5982ac7
timfkl <timfkl11@gmail.com>
2021-02-04 14:41:49 +0000

    Added Responses Model

---
+++ Development/Test/Test/Models/Responses.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Test.Models
+{
+    [Table("responses")]
+    public class Responses
+    {
+        [Key]
+        public int responseID { get; set; }
+        public int questionID { get; set; }
+        public string responseDate { get; set; } = "";
+        public string answersText { get; set; } = "";
+    }
+}
